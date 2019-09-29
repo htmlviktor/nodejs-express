876a0750fe88d644fbb6ac41967eb522292920fe
Viktor Kan <html.viktor@gmail.com>
2019-09-29 21:21:49 +0300

    express -18

--- public/app.js
+++ public/app.js
@@ -1,6 +1,42 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('ru-RU', {
+const toCurrency = price => {
+    return new Intl.NumberFormat('ru-RU', {
         currency: 'rub',
         style: 'currency'
-    }).format(node.textContent);
+    }).format(price);
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent);
 })
+
+const cardContainer = document.querySelector('.card-container');
+
+if (cardContainer) {
+    cardContainer.addEventListener('click', (evt) => {
+        if (evt.target.classList.contains('js-remove')) {
+            const id = evt.target.dataset.id;
+
+
+            fetch(`card/remove/${id}`, {
+                method: 'DELETE',
+            }).then(res => res.json())
+                .then(card => {
+                    if (card.courses.length) {
+                        const html = card.courses.map(c => {
+                            return `<tr>
+                          <td>${c.title}</td>
+                          <td>${c.count}</td>
+                          <td>
+                              <button class="btn btn-small js-remove" data-id="${c.id}">Удалить</button>
+                          </td>
+                      </tr>`
+                        }).join('');
+                    cardContainer.querySelector('tbody').innerHTML = html;
+                    cardContainer.querySelector('.price').textContent = toCurrency(card.price);
+                    } else {
+                        cardContainer.innerHTML = '<p>Все курсы удалены</p>'
+                    }
+                });
+        }
+    })
+}
