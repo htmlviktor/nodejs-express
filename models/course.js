19f9a63b91437de773c69c3da8ddc754f78ab29f
Viktor Kan <html.viktor@gmail.com>
2019-09-29 15:27:11 +0300

    express -16

---
+++ models/course.js
@@ -0,0 +1,84 @@
+const uuid = require('uuid/v4');
+const fs = require('fs');
+const path = require('path');
+
+class Course {
+    constructor(title, price, img) {
+        this.title = title;
+        this.price = price;
+        this.img = img;
+        this.id = uuid();
+    }
+
+    async save() {
+        const courses = await Course.getAll();
+
+        courses.push(this.toJSON());
+        
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                }
+            )
+        })
+    }
+
+    static async update(course) {
+        const courses = await Course.getAll();
+
+        courses[courses.findIndex(c => c.id === course.id)] = course;
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(
+                path.join(__dirname, '..', 'data', 'courses.json'),
+                JSON.stringify(courses),
+                (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                }
+            )
+        })
+    }
+
+    static getAll() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(
+                path.join(__dirname, '..', '/data', 'courses.json'),
+                'utf-8',
+                (err, content) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(JSON.parse(content))
+                    }
+                }
+            )
+        })
+    }
+
+    static async getById(id) {
+        const courses = await Course.getAll();
+        return courses.find(it => it.id === id);
+    }
+
+    toJSON() {
+        return {
+            title: this.title,
+            price: this.price,
+            img: this.img,
+            id: this.id
+        }
+    }
+}
+
+module.exports = Course;
